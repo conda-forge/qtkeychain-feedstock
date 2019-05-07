de7e539b51e7885b09bde5b4c8f9c43cfe966e11
Filipe Fernandes <ocefpaf@gmail.com>
2019-05-07 17:04:52 -0400

    try qt workaround

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
+# Remove the full path from CXX etc. If we don't do this
+# then the full path at build time gets put into 
+# mkspecs/qmodule.pri and qmake attempts to use this.
+export AR=$(basename ${AR})
+export RANLIB=$(basename ${RANLIB})
+export STRIP=$(basename ${STRIP})
+export OBJDUMP=$(basename ${OBJDUMP})
+export CC=$(basename ${CC})
+export CXX=$(basename ${CXX})
+
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  # Avoid Xcode
+  cp "${RECIPE_DIR}"/xcrun .
+  cp "${RECIPE_DIR}"/xcodebuild .
+  # Some test runs 'clang -v', but I do not want to add it as a requirement just for that.
+  ln -s "${CXX}" ${HOST}-clang || true
+  # For ltcg we cannot use libtool (or at least not the macOS 10.9 system one) due to lack of LLVM bitcode support.
+  ln -s "${LIBTOOL}" libtool || true
+  # Just in-case our strip is better than the system one.
+  ln -s "${STRIP}" strip || true
+  chmod +x ${HOST}-clang libtool strip
+  # Qt passes clang flags to LD (e.g. -stdlib=c++)
+  export LD=${CXX}
+  PATH=${PWD}:${PATH}
+fi
+
 [[ -d build ]] || mkdir build
 cd build/
 
 # have to set CMAKE_INSTALL_LIBDIR otherwise it ends up under 'x86_64-linux-gnu'
 
 cmake \
-    -D CMAKE_INSTALL_PREFIX=$PREFIX \
-    -D CMAKE_INSTALL_LIBDIR=$PREFIX/lib \
+    -D CMAKE_INSTALL_PREFIX=${PREFIX} \
+    -D CMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
     ${SRC_DIR}
 
 make -j$CPU_COUNT
