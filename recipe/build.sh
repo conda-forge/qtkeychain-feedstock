94dee97d2f694844a7a0ebb3988643ccbdd8f503
Sam Gillingham <gillingham.sam@gmail.com>
2019-05-10 17:09:26 +1000

    remove commented out section

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
 
-# Remove the full path from CXX etc. If we don't do this
-# then the full path at build time gets put into 
-# mkspecs/qmodule.pri and qmake attempts to use this.
-#export AR=$(basename ${AR})
-#export RANLIB=$(basename ${RANLIB})
-#export STRIP=$(basename ${STRIP})
-#export OBJDUMP=$(basename ${OBJDUMP})
-#export CC=$(basename ${CC})
-#export CXX=$(basename ${CXX})
-
-#if [[ ${HOST} =~ .*darwin.* ]]; then
-  # Some test runs 'clang -v', but I do not want to add it as a requirement just for that.
-#  ln -s "${CXX}" ${HOST}-clang || true
-  # For ltcg we cannot use libtool (or at least not the macOS 10.9 system one) due to lack of LLVM bitcode support.
-#  ln -s "${LIBTOOL}" libtool || true
-  # Just in-case our strip is better than the system one.
-#  ln -s "${STRIP}" strip || true
-#  chmod +x ${HOST}-clang libtool strip
-  # Qt passes clang flags to LD (e.g. -stdlib=c++)
-#  export LD=${CXX}
-#  PATH=${PWD}:${PATH}
-#fi
-
 [[ -d build ]] || mkdir build
 cd build/
 
