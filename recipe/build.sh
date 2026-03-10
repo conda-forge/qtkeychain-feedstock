b4111355cd41f59697bd95e376038889e6af80c2
Alexandre Neto <senhor.neto@gmail.com>
2026-03-10 13:59:27 +0000

    Enable cross-compilation support in build.sh
    Add support for cross-compilation from x86_64 to arm64 by adjusting QT_HOST_PATH based on the CONDA_BUILD_CROSS_COMPILATION environment variable.
    
    This was suggested by AI

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,10 +5,19 @@ cd build/
 
 # have to set CMAKE_INSTALL_LIBDIR otherwise it ends up under 'x86_64-linux-gnu'
 
+# For cross-compilation from x86_64 to arm64
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+    # Use the build platform's qtpaths
+    QT_HOST_PATH="$BUILD_PREFIX"
+else
+    QT_HOST_PATH="$PREFIX"
+fi
+
 cmake ${CMAKE_ARGS} \
     -D CMAKE_INSTALL_PREFIX=${PREFIX} \
     -D CMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
     -D BUILD_WITH_QT6=TRUE \
+    -D QT_HOST_PATH=${QT_HOST_PATH} \
     ${SRC_DIR}
 
 make -j$CPU_COUNT
