3a0b57534c72d1efcc69e8c4f77aeed60c38ef44
Sam Gillingham <gillingham.sam@gmail.com>
2026-03-31 12:40:52 +1000

    cross compilation

--- recipe/build_qt6.sh
+++ recipe/build_qt6.sh
@@ -8,20 +8,21 @@ cd build/
 # have to set CMAKE_INSTALL_LIBDIR otherwise it ends up under 'x86_64-linux-gnu'
 
 # For cross-compilation from x86_64 to arm64
-#if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
-#    # Use the build platform's qtpaths
-#    QT_HOST_PATH="$BUILD_PREFIX"
-#else
-#    QT_HOST_PATH="$PREFIX"
-#fi
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+    # Use the build platform's qtpaths
+    QT_HOST_PATH="$BUILD_PREFIX"
+else
+    QT_HOST_PATH="$PREFIX"
+fi
 
-export PATH=${PREFIX}/lib/qt6/bin:${PATH}
+export PATH=${QT_HOST_PATH}/lib/qt6/bin:${PATH}
 which qtpaths
 
 cmake ${CMAKE_ARGS} \
     -D CMAKE_INSTALL_PREFIX=${PREFIX} \
     -D CMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
     -D BUILD_WITH_QT6=TRUE \
+    -D QT_HOST_PATH=${QT_HOST_PATH} \
     ${SRC_DIR}
 
 make -j$CPU_COUNT
